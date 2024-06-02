4656f8da63fad761fad48ec6ff36b794f1f201b6
losso27 <felipe_losso@hotmail.com>
2024-06-02 19:20:21 -0300

    Add gold logic

--- jason_miners/src/env/example/Env.java
+++ jason_miners/src/env/example/Env.java
@@ -5,6 +5,7 @@ package example;
 import jason.asSyntax.*;
 import jason.environment.*;
 import jason.asSyntax.parser.*;
+import jason.environment.grid.Location;
 
 import java.util.logging.*;
 
@@ -18,7 +19,7 @@ public class Env extends Environment {
     public void init(String[] args) {
         super.init(args);
         try {
-            model = new WorldModel();
+            initWorld();
             addPercept(ASSyntax.parseLiteral("percept("+args[0]+")"));
         } catch (Exception e) {
             e.printStackTrace();
@@ -39,4 +40,58 @@ public class Env extends Environment {
     public void stop() {
         super.stop();
     }
+
+    public void initWorld() {
+        try {
+            model = new WorldModel(); 
+            clearPercepts();
+            addPercept(Literal.parseLiteral("gsize(" + model.getWidth() + "," + model.getHeight() + ")"));
+            updateAgsPercept();
+        } catch (Exception e) {
+            logger.warning("Error creating world "+e);
+        }
+    }
+
+    private void updateAgsPercept() {
+        for (int i = 0; i < 3; i++) {
+            updateAgPercept(i);
+        }
+    }
+
+    private void updateAgPercept(int ag) {
+        updateAgPercept("miner" + (ag + 1), ag);
+    }
+
+    private void updateAgPercept(String agName, int ag) {
+        clearPercepts(agName);
+        // its location
+        Location l = model.getAgPos(ag);
+        addPercept(agName, Literal.parseLiteral("pos(" + l.x + "," + l.y + ")"));
+
+        if (model.isCarryingGold(ag)) {
+            addPercept(agName, Literal.parseLiteral("carrying_gold"));
+        }
+
+        // what's around
+        updateAgPercept(agName, l.x - 1, l.y - 1);
+        updateAgPercept(agName, l.x - 1, l.y);
+        updateAgPercept(agName, l.x - 1, l.y + 1);
+        updateAgPercept(agName, l.x, l.y - 1);
+        updateAgPercept(agName, l.x, l.y);
+        updateAgPercept(agName, l.x, l.y + 1);
+        updateAgPercept(agName, l.x + 1, l.y - 1);
+        updateAgPercept(agName, l.x + 1, l.y);
+        updateAgPercept(agName, l.x + 1, l.y + 1);
+    }
+
+    private void updateAgPercept(String agName, int x, int y) {
+        if (model == null || !model.inGrid(x,y)) return;
+        if (model.hasObject(WorldModel.GOLD, x, y)) {
+            addPercept(agName, Literal.parseLiteral("cell(" + x + "," + y + ",gold)"));
+        }
+        if (model.hasObject(WorldModel.AGENT, x, y)) {
+            addPercept(agName, Literal.parseLiteral("cell(" + x + "," + y + ",ally)"));
+        }
+    }
+
 }
--- jason_miners/src/env/example/WorldModel.java
+++ jason_miners/src/env/example/WorldModel.java
@@ -10,26 +10,32 @@ import java.util.logging.Logger;
 public class WorldModel extends GridWorldModel {
 
     public static final int   GOLD  = 16;
-    public static final int   DEPOT = 32;
 
-    Location                  depot;
+    Set<Integer>              agWithGold;
 
     public WorldModel() throws Exception {
         super(21, 21, 4);
-        this.setDepot(5, 7);
+        agWithGold = new HashSet<Integer>();
         this.setAgPos(0, 1, 0);
         this.setAgPos(1, 20, 0);
         this.setAgPos(2, 3, 20);
         this.setAgPos(3, 20, 20);
         this.setGold(10,10);
     }
-    public void setDepot(int x, int y) {
-        depot = new Location(x, y);
-        data[x][y] = DEPOT;
-    }
 
     public void setGold(int x, int y) {
         data[x][y] = GOLD;
+    }
+
+    public boolean isCarryingGold(int ag) {
+        return agWithGold.contains(ag);
+    }
+
+    public void setAgCarryingGold(int ag) {
+        agWithGold.add(ag);
+    }
+    public void setAgNotCarryingGold(int ag) {
+        agWithGold.remove(ag);
     }
 
 }
